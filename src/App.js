a3e0ecd67b942360dfdef2771c179801b46d16b1
kirtana <kirtana.09kichmbare@gmail.com>
2025-09-15 17:50:20 +0530

    styles changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import LandingPage from "./pages/LandingPage";
-import "./styles/App.css";
+import "./App.css";
 
 function App() {
   return (
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
 import Button from "../components/Button";
-import "./LandingPage.css";
+import ".styles/LandingPage.css";
 
 function LandingPage() {
   return (
