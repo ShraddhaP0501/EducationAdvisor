b026f5d398381b3ef998615df905d7ac88017c8f
Shraddha <shraddhap0501@gmail.com>
2025-09-16 20:50:38 +0530

    about us added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,18 @@ import "../styles/Footer.css";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <p>© 2025 Career Advisor. All rights reserved.</p>
+    <footer>
+      <div>
+        <h3>About Us</h3>
+        <p>
+          Welcome to our website! We are dedicated to providing the best service
+          to our users. Our mission is to make your experience seamless and
+          enjoyable.
+        </p>
+      </div>
+      <div style={{ marginTop: "10px" }}>
+        &copy; {new Date().getFullYear()} Your Company Name. All rights reserved.
+      </div>
     </footer>
   );
 }
